87523ff02944ed56a04071e6e0c10eb0237a82dc
Ed Mckain <mckain.default@gmail.com>
2021-04-30 15:41:05 +0200

    tidies up spaces in the params block

--- plans/promote_modules.pp
+++ plans/promote_modules.pp
@@ -7,13 +7,13 @@ plan cd4pe_plans::promote_modules(
 ){
   $cd4pe_endpoint = "https://${cd4pe_host}"
   $params = {
-    email     => $cd4pe_email,
-    password  => $cd4pe_password.unwrap,
+    email => $cd4pe_email,
+    password => $cd4pe_password.unwrap,
     repo_name => 'control-repo',
     repo_type => 'control',
-    branch_name=>master,
-    workspace=>demo,
-    stage_name=>'Deploy to Production Freeze',
+    branch_name => master,
+    workspace => demo,
+    stage_name => 'Deploy to Production Freeze',
     web_ui_endpoint => $cd4pe_endpoint,
     commit_sha => '737f6e72b818a3c66aa2f5bed51bd3ac0c40d21c',
   }
